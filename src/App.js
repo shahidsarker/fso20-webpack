366f3a8e6dea486ee389291283d3ad2fc5652eb8
Shahid <shahid.sarker@gmail.com>
2020-07-01 22:48:40 -0400

    style-loader css-loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
+import "./index.css";
 
-const App = () => <div>hello webpack</div>;
+const App = () => <div className="container">hello webpack</div>;
 
 export default App;
